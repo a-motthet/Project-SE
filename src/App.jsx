cdd211abf38ca6aa0978a476a20c0d15ccb49252
a-motthet <atsadawut.mott@gmail.com>
2024-10-04 16:45:04 +0700

    RegisterPage Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import './index.css';
-import LoginPage from './pages/LoginPage';
+import "./index.css";
+import LoginPage from "./pages/LoginPage";
+import Home from "./pages/Home";
+import RegisterPage from "./pages/RegisterPage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <div>
-          <LoginPage />
-      </div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Routes>
+      </Router>
     </>
   );
-} 
+}
 
 export default App;
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { IoPersonOutline, IoLockClosedOutline } from "react-icons/io5";
+
+import {
+  IoMailOutline,
+  IoCallOutline,
+  IoPersonOutline,
+  IoLockClosedOutline,
+} from "react-icons/io5";
 import mypic from "../images/1.jpg";
 
-function LoginPage() {
+function RegisterPage() {
   const navigate = useNavigate(); // Initialize the navigate function
 
-  const handleRegister = () => {
-    navigate("/register"); // Navigate to the login page
+  const handleLogin = () => {
+    navigate("/login"); // Navigate to the login page
   };
   return (
     <div className="bg-pupul-bg flex h-screen justify-center items-center">
@@ -15,14 +21,8 @@ function LoginPage() {
 
       <div className="w-1/3 bg-white flex items-center justify-center h-4/6 rounded-r-lg">
         <div className="w-full max-w-md p-8">
-          <div className="flex justify-center mb-7">
-            <img
-              src={mypic}
-              alt="Profile"
-              className="w-64 h-64 rounded-full object-cover"
-            />
-          </div>
           <form>
+            {/* กล่องข้อความ 1 */}
             <div className="mb-3">
               <div className="flex items-center border rounded shadow-sm font-bold">
                 <IoPersonOutline className="text-gray-500 mx-4 size-6" />
@@ -34,7 +34,31 @@ function LoginPage() {
                 />
               </div>
             </div>
-
+            {/* กล่องข้อความ 2 */}
+            <div className="mb-3">
+              <div className="flex items-center border rounded shadow-sm font-bold">
+                <IoCallOutline className="text-gray-500 mx-4 size-6 " />
+                <input
+                  className="w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="phone"
+                  type="tel"
+                  placeholder="Phone"
+                />
+              </div>
+            </div>
+            {/* กล่องข้อความ 3 */}
+            <div className="mb-3">
+              <div className="flex items-center border rounded shadow-sm font-bold">
+                <IoMailOutline className="text-gray-500 mx-4 size-6" />
+                <input
+                  className="w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="email"
+                  type="email"
+                  placeholder="Email"
+                />
+              </div>
+            </div>
+            {/* กล่องข้อความ 4 */}
             <div className="mb-3">
               <div className="flex items-center border rounded shadow-sm font-bold">
                 <IoLockClosedOutline className="text-gray-500 mx-4 size-6" />
@@ -46,32 +70,38 @@ function LoginPage() {
                 />
               </div>
             </div>
-            <div class="flex justify-end">
-              <a
-                className="font-bold text-sm text-blue-500 hover:text-blue-800"
-                href="#"
-              >
-                {" "}
-                ลืมรหัสผ่าน?{" "}
-              </a>
+            {/* กล่องข้อความ 5 */}
+            <div className="mb-3">
+              <div className="flex items-center border rounded shadow-sm font-bold">
+                <IoLockClosedOutline className="text-gray-500 mx-4 size-6" />
+                <input
+                  className="w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="Confirm"
+                  type="password"
+                  placeholder="Confirm Password"
+                />
+              </div>
             </div>
+            {/* ปุ่ม Login สีม่วง */}
             <div class="flex justify-end mt-3">
               <button
                 className="bg-puple-b hover:bg-puple-md text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
                 type="button"
               >
-                Login
+                Register
               </button>
             </div>
+            {/* ข้อความ หรือ */}
             <div class="flex flex-col items-center mt-3">
               <a>- - - - - - - - - - - - - หรือ - - - - - - - - - - - - - -</a>
+              {/* ปุ่ม login */}
               <div class="flex justify-center mt-3">
                 <button
                   className="bg-puple-b hover:bg-puple-md text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
                   type="button"
-                  onClick={handleRegister}
+                  onClick={handleLogin}
                 >
-                  Register
+                  Login
                 </button>
               </div>
             </div>
@@ -82,4 +112,4 @@ function LoginPage() {
   );
 }
 
-export default LoginPage;
+export default RegisterPage;
