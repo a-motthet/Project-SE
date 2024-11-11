a05cabb556a3dcfaa3c8430543dcd15c76e0cc3b
Atsadawut Motthet <atsadawut.mott@gmail.com>
2024-11-12 02:46:55 +0700

    Responsively loginPage

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -4,32 +4,33 @@ import { IoPersonOutline, IoLockClosedOutline } from "react-icons/io5";
 import mypic from "../images/1.jpg";
 
 function LoginPage() {
-  const navigate = useNavigate(); // Initialize the navigate function
+  const navigate = useNavigate();
 
   const handleLogin = () => {
     navigate("/"); // Navigate to the login page
   };
 
   const handleRegister = () => {
-    navigate("/register"); // Navigate to the login page
+    navigate("/register"); // Navigate to the register page
   };
+
   return (
-    <div className="bg-pupul-bg flex h-screen justify-center items-center">
+    <div className="bg-pupul-bg flex h-screen justify-center items-center px-4">
       <div className="w-1/3 bg-gradient-to-t from-puple-b to-puple-md h-4/6 rounded-l-lg"></div>
 
       <div className="w-1/3 bg-white flex items-center justify-center h-4/6 rounded-r-lg">
-        <div className="w-full max-w-md p-8">
+        <div className="w-full p-6 sm:p-8">
           <div className="flex justify-center mb-7">
             <img
               src={mypic}
               alt="Profile"
-              className="w-64 h-64 rounded-full object-cover"
+              className="w-32 h-32 md:w-48 md:h-48 rounded-full object-cover"
             />
           </div>
           <form>
             <div className="mb-3">
               <div className="flex items-center border rounded shadow-sm font-bold">
-                <IoPersonOutline className="text-gray-500 mx-4 size-6" />
+                <IoPersonOutline className="text-gray-500 mx-4 text-xl sm:text-2xl" />
                 <input
                   className="w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                   id="username"
@@ -41,7 +42,7 @@ function LoginPage() {
 
             <div className="mb-3">
               <div className="flex items-center border rounded shadow-sm font-bold">
-                <IoLockClosedOutline className="text-gray-500 mx-4 size-6" />
+                <IoLockClosedOutline className="text-gray-500 mx-4 text-xl sm:text-2xl" />
                 <input
                   className="w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                   id="password"
@@ -50,16 +51,15 @@ function LoginPage() {
                 />
               </div>
             </div>
-            <div class="flex justify-end">
+            <div className="flex justify-end">
               <a
                 className="font-bold text-sm text-blue-500 hover:text-blue-800"
                 href="#"
               >
-                {" "}
-                ลืมรหัสผ่าน?{" "}
+                ลืมรหัสผ่าน?
               </a>
             </div>
-            <div class="flex justify-end mt-3">
+            <div className="flex justify-end mt-3">
               <button
                 className="bg-puple-b hover:bg-puple-md text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
                 type="button"
@@ -68,9 +68,9 @@ function LoginPage() {
                 Login
               </button>
             </div>
-            <div class="flex flex-col items-center mt-3">
+            <div className="flex flex-col items-center mt-3">
               <a>- - - - - - - - - - - - - หรือ - - - - - - - - - - - - - -</a>
-              <div class="flex justify-center mt-3">
+              <div className="flex justify-center mt-3">
                 <button
                   className="bg-puple-b hover:bg-puple-md text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
                   type="button"
