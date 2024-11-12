6a41792c2a213884fca9311697b2641879d1e2d1
Tharathon <grrhkm72@gmail.com>
2024-11-13 02:49:33 +0700

    ?

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,7 +19,7 @@ function Home() {
         </div>
         <div>
           <h4 className="text-center text-[45px] font-bold font-sans my-8  font-chela ">
-            WHY ANIMALOVER ?
+            WHY ANIMALOVER 
           </h4>
           <Why />
         </div>
