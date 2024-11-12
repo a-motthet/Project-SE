3cfb241e71d8c166ecde7cf10ea25794b6840a31
Tharathon <grrhkm72@gmail.com>
2024-11-13 02:34:52 +0700

    detail

--- src/pages/Ex.jsx
+++ src/pages/Ex.jsx
@@ -1,79 +1,69 @@
-import React from 'react';
-import mypic from '../images/2.jpg';
+import React from "react";
+import mypic from "../images/2.jpg";
 
 function Ex() {
   return (
-    <div className="flex flex-col min-h-screen bg-[#e9e1f1]">
+    <>
+      <div className="bg-white h-[800px]">
+        <header className="mt-8 p-8">
+          <h1 className="text-[30px] my-10 font-bold font-sans text-center text-puple-b">
+            ประสบการณ์จากครอบครัว Animalover
+          </h1>
+        </header>
 
-    
-      <header className="mt-8 p-4">
-        <h1 className="text-3xl font-bold text-center text-[#6373B7] mb-5">ประสบการณ์จากครอบครัว Animalover</h1>
-      </header>
+        <main className="flex-grow flex justify-center items-center p-4">
+          <div className="flex gap-6 justify-center flex-wrap">
+            <div className="flex items-start w-80 h-96 bg-pupul-bg rounded-lg shadow-lg cursor-pointer p-4 transform transition-transform duration-300 hover:scale-105 hover:shadow-xl">
+              <img
+                src={mypic}
+                alt="Profile"
+                className="w-16 h-16 rounded-full object-cover"
+              />
+              <div className="ml-4">
+                <h2 className="text-xl font-medium text-puple-b">Tha</h2>
+                <p className="text-gray-500">❤️❤️❤️❤️</p>
+              </div>
+            </div>
 
-  
-      <main className="flex-grow flex justify-center items-center p-4">
-        <div className="flex gap-6 justify-center flex-wrap">
-          
-          
-          <div className="flex items-start w-80 h-96 bg-white rounded-lg shadow-lg cursor-pointer p-4 transform transition-transform duration-300 hover:scale-105 hover:shadow-xl">
-            <img
-              src={mypic}
-              alt="Profile"
-              className="w-16 h-16 rounded-full object-cover"
-            />
-            <div className="ml-4">
-              <h2 className="text-xl font-medium text-[#6373B7]">Tha</h2>
-              <p className="text-gray-500">❤️❤️❤️❤️</p>
+            <div className="flex items-start w-80 h-96 bg-pupul-bg rounded-lg shadow-lg cursor-pointer p-4 transform transition-transform duration-300 hover:scale-105 hover:shadow-xl">
+              <img
+                src={mypic}
+                alt="Profile"
+                className="w-16 h-16 rounded-full object-cover"
+              />
+              <div className="ml-4">
+                <h2 className="text-xl font-medium text-puple-b">NanS</h2>
+                <p className="text-gray-500">❤️❤️❤️❤️</p>
+              </div>
+            </div>
+
+            <div className="flex items-start w-80 h-96 bg-pupul-bg rounded-lg shadow-lg cursor-pointer p-4 transform transition-transform duration-300 hover:scale-105 hover:shadow-xl">
+              <img
+                src={mypic}
+                alt="Profile"
+                className="w-16 h-16 rounded-full object-cover"
+              />
+              <div className="ml-4">
+                <h2 className="text-xl font-medium text-puple-b">JuJuu</h2>
+                <p className="text-gray-500">❤️❤️❤️❤️</p>
+              </div>
+            </div>
+
+            <div className="flex items-start w-80 h-96 bg-pupul-bg rounded-lg shadow-lg cursor-pointer p-4 transform transition-transform duration-300 hover:scale-105 hover:shadow-xl">
+              <img
+                src={mypic}
+                alt="Profile"
+                className="w-16 h-16 rounded-full object-cover"
+              />
+              <div className="ml-4">
+                <h2 className="text-xl font-medium text-puple-b">gew</h2>
+                <p className="text-gray-500">❤️❤️❤️❤️</p>
+              </div>
             </div>
           </div>
-          
-          
-          <div className="flex items-start w-80 h-96 bg-white rounded-lg shadow-lg cursor-pointer p-4 transform transition-transform duration-300 hover:scale-105 hover:shadow-xl">
-            <img
-              src={mypic}
-              alt="Profile"
-              className="w-16 h-16 rounded-full object-cover"
-            />
-            <div className="ml-4">
-              <h2 className="text-xl font-medium text-[#6373B7]">NanS</h2>
-              <p className="text-gray-500">❤️❤️❤️❤️</p>
-            </div>
-          </div>
-          
-          
-          <div className="flex items-start w-80 h-96 bg-white rounded-lg shadow-lg cursor-pointer p-4 transform transition-transform duration-300 hover:scale-105 hover:shadow-xl">
-            <img
-              src={mypic}
-              alt="Profile"
-              className="w-16 h-16 rounded-full object-cover"
-            />
-            <div className="ml-4">
-              <h2 className="text-xl font-medium text-[#6373B7]">JuJuu</h2>
-              <p className="text-gray-500">❤️❤️❤️❤️</p>
-            </div>
-          </div>
-
-          
-          <div className="flex items-start w-80 h-96 bg-white rounded-lg shadow-lg cursor-pointer p-4 transform transition-transform duration-300 hover:scale-105 hover:shadow-xl">
-            <img
-              src={mypic}
-              alt="Profile"
-              className="w-16 h-16 rounded-full object-cover"
-            />
-            <div className="ml-4">
-              <h2 className="text-xl font-medium text-[#6373B7]">gew</h2>
-              <p className="text-gray-500">❤️❤️❤️❤️</p>
-            </div>
-          </div>
-
-        </div>
-      </main>
-
-      {/* Footer */}
-      <footer className="bg-[#6373B7] py-6 text-white text-center">
-        <p className="text-sm">© 2023 Animalover. All rights reserved.</p>
-      </footer>
-    </div>
+        </main>
+      </div>
+    </>
   );
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,9 +12,15 @@ function Home() {
           <Welcome />
         </div>
         <div>
+          <h4 className="ml-[320px] text-[24px] font-bold font-sans">
+            รายการทั้งหมด
+          </h4>
           <Recomend />
         </div>
         <div>
+          <h4 className="text-center text-[45px] font-bold font-sans my-8  font-chela ">
+            WHY ANIMALOVER ?
+          </h4>
           <Why />
         </div>
         <div>
