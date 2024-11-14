f5112b1b4f8577916832513740f5e063186b69f0
Atsadawut Motthet <Atsadawut.mott@gmail.com>
2024-11-14 23:15:43 +0700

    Homepage Done

---
+++ src/constants/index.js
@@ -0,0 +1,33 @@
+ 
+export const ServiceData = [
+    {
+      title: "JuJuu",
+      content: "1",
+
+    },
+    {
+      title: "Nanxi",
+      content: "2",
+
+    },
+    {
+      title: "Gew",
+      content: "3",
+
+    },
+    {
+      title: "Pond",
+      content: "4",
+
+    },
+    {
+      title: "Nook",
+      content: "5",
+
+    },
+    {
+      title: "Boss boss",
+      content: "6",
+
+    },
+  ];
